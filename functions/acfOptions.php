19b48a7cdea8f4506d04b5209bec35124507ffe7
David Hellmann <davidhellmann.com@gmail.com>
2015-12-01 09:57:42 +0100

    Add File

---
+++ functions/acfOptions.php
@@ -0,0 +1,13 @@
+<?php
+
+  if( function_exists('acf_add_options_page') ) {
+
+    acf_add_options_page();
+
+    acf_add_options_sub_page('Stage Slider');
+    acf_add_options_sub_page('Stores');
+    acf_add_options_sub_page('Red Bull Worlds');
+
+  }
+
+?>
